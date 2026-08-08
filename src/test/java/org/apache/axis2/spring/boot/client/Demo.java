ca5e97dbdeb38727f48bd5492b9a4b3ac35bdc6f
wandl-6A72h <hiwepy@gmail.com>
2026-08-08 23:20:01 +0800

    chore: propagate pom standardization, tests, javadoc and @author fix from 4.1.x
    - Add jacoco-maven-plugin 0.8.15 with 90% coverage gate
    - Add central-publishing-maven-plugin 0.11.0
    - Update surefire (enable tests, argLine for JaCoCo, *_Test.java pattern)
    - Fix SCM URLs
    - Copy test files from 4.1.x branch
    - Copy Javadoc improvements from 4.1.x branch
    - Standardize @author to [@Loong Wan](https://github.com/loong10k)
    - Propagated to 3.5.x branch

--- src/test/java/org/apache/axis2/spring/boot/client/Demo.java
+++ src/test/java/org/apache/axis2/spring/boot/client/Demo.java
@@ -17,7 +17,7 @@ package org.apache.axis2.spring.boot.client;
 
 /**
  * TODO
- * @author 		： <a href="https://github.com/hiwepy">hiwepy</a>
+ * @author [@Loong Wan](https://github.com/loong10k)
  */
 import java.util.Iterator;
 
