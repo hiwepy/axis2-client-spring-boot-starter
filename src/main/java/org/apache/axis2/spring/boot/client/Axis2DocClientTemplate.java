9be46d0d5ba3f4d838b7973083e5d4df82fc956b
wandl-6A72h <hiwepy@gmail.com>
2026-08-08 23:20:07 +0800

    chore: propagate pom standardization, tests, javadoc and @author fix from 4.1.x
    - Add jacoco-maven-plugin 0.8.15 with 90% coverage gate
    - Add central-publishing-maven-plugin 0.11.0
    - Update surefire (enable tests, argLine for JaCoCo, *_Test.java pattern)
    - Fix SCM URLs
    - Copy test files from 4.1.x branch
    - Copy Javadoc improvements from 4.1.x branch
    - Standardize @author to [@Loong Wan](https://github.com/loong10k)
    - Propagated to 4.0.x branch

--- src/main/java/org/apache/axis2/spring/boot/client/Axis2DocClientTemplate.java
+++ src/main/java/org/apache/axis2/spring/boot/client/Axis2DocClientTemplate.java
@@ -32,7 +32,7 @@ import org.apache.axis2.client.async.AxisCallback;
 
 /**
  * TODO
- * @author 		： <a href="https://github.com/hiwepy">hiwepy</a>
+ * @author [@Loong Wan](https://github.com/loong10k)
  */
 public class Axis2DocClientTemplate {
 	
