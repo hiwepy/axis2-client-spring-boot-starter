8ffd0fac9f018c3c4c911cc2090c5cddb179ad3c
loong10k <20489781+loong10k@users.noreply.github.com>
2026-08-10 16:19:43 +0800

    docs: unify @author tag to @author <a href="https://github.com/loong10k">Loong Wan</a>

--- src/main/java/org/apache/axis2/spring/boot/client/Axis2ClientOptions.java
+++ src/main/java/org/apache/axis2/spring/boot/client/Axis2ClientOptions.java
@@ -21,7 +21,7 @@ import java.util.Map;
 /**
  * TODO
  * 
- * @author [@Loong Wan](https://github.com/loong10k)
+ * @author <a href="https://github.com/loong10k">Loong Wan</a>
  */
 public class Axis2ClientOptions {
 
